948414fcb3420fb330bf2c5a006a0acd2d65b80f
Phil Hawksworth <phil@netify.com>
2018-01-15 14:13:53 +0000

    repsonse debugging

--- src/lambda/puttr.js
+++ src/lambda/puttr.js
@@ -23,7 +23,7 @@ export function handler(event, context, callback) {
     console.log(hits.length + " results returned");
     callback(null, {
       statusCode: 200,
-      body: {"results": hits }
+      body: JSON.stringify(hits)
     })
   });
 
